bb89696db9c29d275faec27ad683e8cdff1a1e68
Emre Erkmen <erkmene@gmail.com>
2022-12-06 10:28:35 +0300

    Day 6

--- 06.js
+++ 06.js
@@ -5,7 +5,7 @@ const parseData = (file) => fs.readFileSync(file).toString().trim();
 
 const testData = fs.readFileSync("06.test.dat").toString().trim().split("\n");
 
-const findEndOfPacketMarker = (str, markerLength = 4) => {
+const findEndOfMarker = (str, markerLength = 4) => {
   for (let i = markerLength - 1; i < str.length; i++) {
     const prev = new Set(str.slice(i - markerLength + 1, i));
     if (prev.size === markerLength - 1) {
@@ -17,24 +17,24 @@ const findEndOfPacketMarker = (str, markerLength = 4) => {
   }
 };
 
-assert.equal(findEndOfPacketMarker(testData[0]), 7);
-assert.equal(findEndOfPacketMarker(testData[1]), 5);
-assert.equal(findEndOfPacketMarker(testData[2]), 6);
-assert.equal(findEndOfPacketMarker(testData[3]), 10);
-assert.equal(findEndOfPacketMarker(testData[4]), 11);
+assert.equal(findEndOfMarker(testData[0]), 7);
+assert.equal(findEndOfMarker(testData[1]), 5);
+assert.equal(findEndOfMarker(testData[2]), 6);
+assert.equal(findEndOfMarker(testData[3]), 10);
+assert.equal(findEndOfMarker(testData[4]), 11);
 
-assert.equal(findEndOfPacketMarker(testData[0], 14), 19);
-assert.equal(findEndOfPacketMarker(testData[1], 14), 23);
-assert.equal(findEndOfPacketMarker(testData[2], 14), 23);
-assert.equal(findEndOfPacketMarker(testData[3], 14), 29);
-assert.equal(findEndOfPacketMarker(testData[4], 14), 26);
+assert.equal(findEndOfMarker(testData[0], 14), 19);
+assert.equal(findEndOfMarker(testData[1], 14), 23);
+assert.equal(findEndOfMarker(testData[2], 14), 23);
+assert.equal(findEndOfMarker(testData[3], 14), 29);
+assert.equal(findEndOfMarker(testData[4], 14), 26);
 
 const data = parseData("06.dat");
 
 console.time("Time to first answer");
-console.log("First Answer", findEndOfPacketMarker(data));
+console.log("First Answer", findEndOfMarker(data));
 console.timeEnd("Time to first answer");
 
 console.time("Time to second answer");
-console.log("Second Answer", findEndOfPacketMarker(data, 14));
+console.log("Second Answer", findEndOfMarker(data, 14));
 console.timeEnd("Time to second answer");
